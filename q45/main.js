abcded1fc908c8cb9a5aa767c8ae8a2fa7c9c49d
Fozia07 <fozia.mustafa07@gmail.com>
2024-06-18 16:18:19 +0500

    add changes

--- q45/main.js
+++ q45/main.js
@@ -1,16 +1,11 @@
 "use strict";
 // Q45 :Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name.
 //  It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs,
-//   such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+//   such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly
 function car_feature(manufacturer, model, ...options) {
-    let car = { manufacturer: manufacturer,
-        model: model };
-    //addtional option 
-    options.forEach(option => {
-        let [key, value] = option.split(":");
-        [key] = value;
-    });
+    let car = { manufacturer, model };
+    options.forEach(([key, value]) => (car[key] = value));
     return car;
 }
-console.log(car_feature("kia", "picanto", "color:silver", "year:2020"));
-console.log(car_feature("honda", "city", "color:red", "year:2018"));
+console.log(car_feature("kia", "picanto", ["colour", "white"], ["sunroof", "true"]));
+console.log(car_feature("honda", "city", ["color", "red"], ["year", "2018"]));
